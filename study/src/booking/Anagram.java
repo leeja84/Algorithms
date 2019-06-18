74b0e5b080f0b51ebe2fb7ee8067e9fa3091aa23
Jaeyoung <leeja84@gmail.com>
2019-06-19 01:31:01 +0200

    add anagram

--- study/src/booking/Anagram.java
+++ study/src/booking/Anagram.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * 
@@ -41,29 +43,56 @@ public class Anagram {
 		System.out.println(ana());
 	}
 
-	public static List<List<String>> ana() {
-		List<String> list = new ArrayList<>();
+	public static String ana() {
 		String[] array = { "pear", "dormitory", "tinsel", "dirty room", "hamlet", "listen", "amleth", "silnet" };
 
+		String result = "";
+
 		if (array == null || array.length == 0) {
-			return new ArrayList<List<String>>();
+			return result;
 		}
 
-		Map<String, List<String>> map = new HashMap<String, List<String>>();
+		Map<String, List<String>> anagramMap = new HashMap<String, List<String>>();
 		for (String s : array) {
+			// 각 스트링값을 정렬한거를 키 값으로사용함.
 			char[] ca = s.toCharArray();
-
 			Arrays.sort(ca);
+			String compareStr = String.valueOf(ca).trim();
 
-			String keyStr = String.valueOf(ca);
-			if (!map.containsKey(keyStr)) {
-				map.put(keyStr, new ArrayList<String>());
+			// 기존에 존재하지 않았던 값인지 확인해서 객체를 생성. 매번 해당 키에 값 추가.
+			if (!anagramMap.containsKey(compareStr)) {
+				anagramMap.put(compareStr, new ArrayList<String>());
 			}
-			map.get(keyStr).add(s);
-			for (String key : map.keySet()) {
-				Collections.sort(map.get(key));
-			}
+			anagramMap.get(compareStr).add(s);
+
 		}
-		return new ArrayList<List<String>>(map.values());
+
+		Map<String, List<String>> sortedMap = new TreeMap<String, List<String>>();
+
+		for (String key : anagramMap.keySet()) {
+			List<String> list = anagramMap.get(key);
+			
+			Collections.sort(list);
+			sortedMap.put(list.get(0), list);
+		}
+
+		
+		// make output.
+		for (String key : sortedMap.keySet()) {
+
+			List<String> list = sortedMap.get(key);
+
+			for (int i = 0; i < list.size(); i++) {
+				result += list.get(i);
+				if (i < list.size() - 1) {
+					result += ", ";
+				}
+			}
+			result += "\n";
+		}
+
+		result = result.substring(0, result.length() - 1);
+
+		return result;
 	}
 }
